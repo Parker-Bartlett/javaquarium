543db5b73249045e672d74596c435fb51de57eea
Parker-Bartlett <prbartlett1@gmail.com>
2019-03-30 00:00:43 -0400

    initial build

---
+++ javaquarium-api/src/main/java/com/javaquarium/javaquarium/JavaquariumApplication.java
@@ -0,0 +1,13 @@
+package com.javaquarium.javaquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaquariumApplication.class, args);
+	}
+
+}
